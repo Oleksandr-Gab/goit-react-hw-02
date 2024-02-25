db8632810db2025912a5e2f5614a8385237d23fc
Olexsandr <Saniafan1@gmail.com>
2024-02-25 15:58:31 +0200

    light css

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,12 @@
+import css from "./Options.module.css";
+
 export default function Options({ totalFeedback, onUpdate, onReset }) {
     return (
-        <>
+        <div className={css.btnContainer}>
             <button onClick={() => onUpdate("good")}>Good</button>
             <button onClick={() => onUpdate("neutral")}>Neutral</button>
             <button onClick={() => onUpdate("bad")}>Bad</button>
             {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
-        </>
+        </div>
     );
 }
